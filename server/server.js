f33724f9139d194d0738f2d68158fd0e49ffe340
hberrino <berrinohernan@gmail.com>
2026-01-08 16:07:52 -0300

    api express mongo

--- server/server.js
+++ server/server.js
@@ -9,25 +9,22 @@ dotenv.config();
 
 const app = express();
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
 
-// Rutas
+
 app.use("/api/vehicles", vehicleRoutes);
 
-// Ruta test
 app.get("/", (req, res) => {
   res.send("API Parking funcionando");
 });
 
-// MongoDB
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB conectado"))
   .catch((err) => console.error("Error MongoDB:", err));
 
-// Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`Backend corriendo en puerto ${PORT}`)
--- src/data/data.js
+++ src/data/data.js
@@ -1,16 +1,10 @@
-// src/data/data.js
-
-// URL de la API
 export const API_URL = "http://localhost:5000/api/vehicles";
 
-// Regex para validar patente
 export const PATENTE_REGEX = /^[A-Za-z0-9]{5,8}$/;
 
-// Tarifas por defecto
 export const TARIFAS_DEFAULT = {
   precioMoto: 300,
   precioAuto: 500,
 };
 
-// Tamaño por defecto del estacionamiento
 export const ESPACIOS_DEFAULT = 20;
