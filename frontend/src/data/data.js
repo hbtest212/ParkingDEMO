5881efd7194a28a998c0a63aaa43cf64864c5c5e
hberrino <berrinohernan@gmail.com>
2026-01-12 17:51:07 -0300

    front end rdy

--- frontend/src/data/data.js
+++ frontend/src/data/data.js
@@ -1,12 +1,13 @@
 export const PATENTE_REGEX = /^[A-Za-z0-9]{5,8}$/;
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const VEHICLES_URL = `${API_URL}/api/vehicles`;
+export const SETTINGS_URL = `${API_URL}/api/settings`;
+export const LOGS_URL = `${API_URL}/api/logs`;
+
 
 export const TARIFAS_DEFAULT = {
   precioMoto: 300,
   precioAuto: 500,
 };
-
 export const ESPACIOS_DEFAULT = 30;
-export const VEHICLES_URL = "http://localhost:3000/api/vehicles";
-export const SETTINGS_URL = "http://localhost:3000/api/settings";
-export const LOGS_URL = "http://localhost:3000/api/logs";
-
